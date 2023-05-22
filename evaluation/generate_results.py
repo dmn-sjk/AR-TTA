7c516adcffbf226a25b9d97f2aa85490f8ba08e6
dmn-sjk <sojka.damian9@gmail.com>
2023-05-22 21:39:57 +0200

    update generate results script

--- evaluation/generate_results.py
+++ evaluation/generate_results.py
@@ -32,14 +32,59 @@ def get_plot_color(method):
     elif 'eata' in method and 'tab:orange' not in colors.values():
         colors[method] = 'tab:orange'
         return 'tab:orange' 
+    
+    # COTTA mods
+    # elif 'no_stochastic_res' in method:
+    #     colors[method] = 'greenyellow'
+    #     return 'greenyellow'
+    # elif 'distilltemp4_out' in method:
+    #     colors[method] = 'blueviolet'
+    #     return 'blueviolet'
+    # elif 'distilltemp2_out' in method:
+    #     colors[method] = 'deepskyblue'
+    #     return 'deepskyblue'
+    # elif 'distill_feat_with_loss' in method:
+    #     colors[method] = 'deeppink'
+    #     return 'deeppink'
+    # elif 'distill_feat_without_loss' in method:
+    #     colors[method] = 'teal'
+    #     return 'teal'
+    # elif 'no_teacher' in method:
+    #     colors[method] = 'peru'
+    #     return 'peru'
+    
     elif 'cotta' in method and 'tab:blue' not in colors.values():
         colors[method] = 'tab:blue'
         return 'tab:blue' 
     elif 'tent' in method and 'tab:purple' not in colors.values():
         colors[method] = 'tab:purple'
-        return 'tab:purple' 
+        return 'tab:purple'
+    
+    # SAR mods
+    # elif 'unifoptim_nores' in method:
+    #     colors[method] = 'greenyellow'
+    #     return 'greenyellow'  
+    # elif 'sar_setup_nores' in method:
+    #     colors[method] = 'blueviolet'
+    #     return 'blueviolet'
+    # elif 'sar_setup' in method:
+    #     colors[method] = 'deepskyblue'
+    #     return 'deepskyblue'
+    # elif 'frozen_resnet50gn_gn' in method:
+    #     colors[method] = 'deeppink'
+    #     return 'deeppink' 
+    
+    
+    elif 'sar' in method and 'tab:brown' not in colors.values():
+        colors[method] = 'tab:brown'
+        return 'tab:brown' 
 
-    random_color = np.random.choice(list(mcolors.TABLEAU_COLORS)[5:])
+
+    while True:
+        random_color = np.random.choice(list(mcolors.CSS4_COLORS))
+        if random_color not in colors.values() and 'white' not in random_color:
+            break
+
     colors[method] = random_color
     return random_color
 
@@ -188,13 +233,22 @@ def plot_acc_train(results, domains, args):
         
 def plot_per_class_acc(results, domains, args):
     result_key = "Top1_ClassAcc_Epoch/train_phase/train_stream/"
+    
+    logs_with_per_class_acc_data = []
+    for method in LOGS_TO_USE:
+        check_class_id = 0
+        if result_key + str(check_class_id) in results[method].keys():
+            logs_with_per_class_acc_data.append(method)
+        else:
+            print(f"No per class accuracy data in for {method}!")
+    
     max_cols = 3
-    nrows = int(np.ceil(len(LOGS_TO_USE) / max_cols))
-    ncols = max_cols if len(LOGS_TO_USE) >= max_cols else len(LOGS_TO_USE)
+    nrows = int(np.ceil(len(logs_with_per_class_acc_data) / max_cols))
+    ncols = max_cols if len(logs_with_per_class_acc_data) >= max_cols else len(logs_with_per_class_acc_data)
     fig, axs = plt.subplots(nrows=nrows,
                             ncols=ncols, figsize=(15, 10))
 
-    for i, method in enumerate(LOGS_TO_USE):
+    for i, method in enumerate(logs_with_per_class_acc_data):
         if nrows > 1 and ncols > 1:  
             row = i // max_cols
             col = i % max_cols
@@ -206,13 +260,18 @@ def plot_per_class_acc(results, domains, args):
             ax = axs[col]
         
         class_id = 0
+        # while True, because of lack of information about class num
         while True:
             if result_key + str(class_id) not in results[method].keys():
                 break
 
-            accs = np.array(results[method][result_key + str(class_id)]) * 100
-            ax.plot(range(len(accs)), accs, 
-                               marker='.', label=class_id)
+                
+            accs_array = np.array(results[method][result_key + str(class_id)])
+
+            # multiply only elements which are not None 
+            # (in CLAD not all classes are in every sequence so the acc is none)
+            accs = np.multiply(accs_array, np.full_like(accs_array, 100), where=accs_array!=None)
+            ax.plot(range(len(accs)), accs, marker='.', label=class_id)
 
             class_id += 1
 
@@ -222,6 +281,7 @@ def plot_per_class_acc(results, domains, args):
         ax.grid(axis='both')
         ax.set_xlabel("Domain")
         ax.set_ylabel("Accuracy [%]")
+        ax.set_ylim(0, 100)
 
         plt.tight_layout()
         plt.subplots_adjust(top=0.9)
@@ -239,13 +299,23 @@ def plot_per_class_acc(results, domains, args):
 def plot_pred_class_ratio(results, domains, args):
     per_class_samples_key = "per_class_samples"
     per_class_predictions_key = "per_class_predictions"
+    
+    logs_with_per_class_data = []
+    for method in LOGS_TO_USE:
+        if per_class_samples_key in results[method].keys():
+            logs_with_per_class_data.append(method)
+        else:
+            print(f"No per_class_samples and per_class_predictions data for {method}!")
+    
     max_cols = 3
-    nrows = int(np.ceil(len(LOGS_TO_USE) / max_cols))
-    ncols = max_cols if len(LOGS_TO_USE) >= max_cols else len(LOGS_TO_USE)
+    nrows = int(np.ceil(len(logs_with_per_class_data) / max_cols))
+    ncols = max_cols if len(logs_with_per_class_data) >= max_cols else len(logs_with_per_class_data)
     fig, axs = plt.subplots(nrows=nrows,
                             ncols=ncols, figsize=(15, 10))
 
-    for i, method in enumerate(LOGS_TO_USE):
+    max_y_val = -1e6
+    min_y_val = 1e6
+    for i, method in enumerate(logs_with_per_class_data):
         if nrows > 1 and ncols > 1:  
             row = i // max_cols
             col = i % max_cols
@@ -261,6 +331,10 @@ def plot_pred_class_ratio(results, domains, args):
         preds_samples_ratio = np.divide(per_class_predictions, per_class_samples, 
                                         out=np.full_like(per_class_predictions, fill_value=None,),
                                         where=per_class_samples != 0)
+        curr_max_val = np.nanmax(preds_samples_ratio)
+        curr_min_val = np.nanmin(preds_samples_ratio)
+        if curr_max_val > max_y_val: max_y_val = curr_max_val
+        if curr_min_val < min_y_val: min_y_val = curr_min_val
         
         num_classes = len(per_class_samples[0])
 
@@ -284,6 +358,9 @@ def plot_pred_class_ratio(results, domains, args):
         fig.legend(handles, labels, loc='lower center')
         
         fig.suptitle("Per class preds vs samples ratio", fontsize="x-large")
+        
+    for ax in axs.reshape(-1):
+        ax.set_ylim(min_y_val, max_y_val)
 
     if args.save_results:
         plt.savefig(os.path.join(RESULTS_FOLDER, args.results_name, 'per_class_preds_samples_ratio'))
@@ -293,6 +370,7 @@ def plot_pred_class_ratio(results, domains, args):
 def plot_plot_per_timeofday_acc(results, domains, args):
     fig, ax = plt.subplots(figsize=(10, 7))
     avg_accs = {'day': [], 'night': []}
+    print("\nDay vs night results")
     for method in LOGS_TO_USE:
         sum_day, sum_night, num_day, num_night = 0, 0, 0, 0
         for domain, accs in zip(domains, results[method]["Top1_Acc_MB/train_phase/train_stream"]):
@@ -308,6 +386,9 @@ def plot_plot_per_timeofday_acc(results, domains, args):
 
         avg_accs['day'].append((sum_day / num_day) * 100)
         avg_accs['night'].append((sum_night / num_night) * 100)
+        
+        print(f"- {method}: \tAcc day: {avg_accs['day'][-1]:.1f}, \tAcc night: {avg_accs['night'][-1]:.1f}")
+        print(f"  {' ' * len(method)}  \tClass error day: {100 - avg_accs['day'][-1]:.1f}, \tClass error night: {100 - avg_accs['night'][-1]:.1f}")
 
     width = 0.3
     plt.xticks(range(len(LOGS_TO_USE)), LOGS_TO_USE, rotation=45)
@@ -324,6 +405,48 @@ def plot_plot_per_timeofday_acc(results, domains, args):
         plt.savefig(os.path.join(RESULTS_FOLDER, args.results_name, 'avg_train_acc_and_time.png'))
     else:
         plt.show()
+
+def frozen_related_results_plot(per_domain_avg_accs_train, domains, args):
+    fig, ax = plt.subplots(figsize=(10, 7))
+    
+    frozen_method_log = ''
+    for method in LOGS_TO_USE:
+        if 'frozen' in method:
+            frozen_method_log = method
+    
+    ys = []
+    for method in LOGS_TO_USE:
+        if method == frozen_method_log:
+            continue
+        
+        subtraction = np.subtract(per_domain_avg_accs_train[method],
+                                  per_domain_avg_accs_train[frozen_method_log])
+        ax.scatter(per_domain_avg_accs_train[frozen_method_log],
+                   subtraction,
+                   label=method,
+                   color=get_plot_color(method))
+        ys.extend(subtraction)
+        
+    xs = per_domain_avg_accs_train[frozen_method_log] * (len(LOGS_TO_USE) - 1)
+    # calculate equation for trendline
+    z = np.polyfit(xs, ys, 1)
+    p = np.poly1d(z)
+
+    # add trendline to plot
+    ax.plot(xs, p(xs), 'y--', label='Trend')
+
+    ax.set_title("Frozen-related accuracy")
+    ax.set_xlabel("Frozen avg acc [%]")
+    ax.set_ylabel("method avg acc - frozen avg acc [%]")
+    ax.grid(axis='both')
+    plt.legend(loc='best')
+    plt.tight_layout()
+    
+    if args.save_results:
+        plt.savefig(os.path.join(RESULTS_FOLDER, args.results_name, 'frozen_related_results.png'))
+    else:
+        plt.show()
+    
         
 def main(args):
     if args.save_results:
@@ -338,17 +461,23 @@ def main(args):
     # =======================================================================================
     table = [["method", *domains, "Avg"], *[[] for i in range(len(LOGS_TO_USE))]]
     avg_accs_train = []
-    print("Average acc train:")
+    per_domain_avg_accs_train = {method: [] for method in LOGS_TO_USE}
     for i, method in enumerate(LOGS_TO_USE):
         table[i + 1].append(method)
         flattened_results = []
-        for single_domain in results[method]["Top1_Acc_MB/train_phase/train_stream"]:
-            table[i + 1].append(np.mean(single_domain) * 100.0)
+        for j, single_domain in enumerate(results[method]["Top1_Acc_MB/train_phase/train_stream"]):
+
+            # if domains[j] in domains[:j]:
+            #     break
+            
+            single_domain_avg_acc = np.mean(single_domain) * 100.0
+            per_domain_avg_accs_train[method].append(single_domain_avg_acc)
+            table[i + 1].append(single_domain_avg_acc)
             flattened_results.extend(single_domain)
 
         avg_acc_train = np.mean(flattened_results) * 100.0
 
-        print(f'- {method}: {avg_acc_train:.2f}')
+        print(f'- {method}: \tAcc: {avg_acc_train:.1f}, \tClassification error: {100 - avg_acc_train:.1f}')
         table[i + 1].append(avg_acc_train)
         
         avg_accs_train.append(avg_acc_train)
@@ -372,8 +501,11 @@ def main(args):
         plot_pred_class_ratio(results, domains, args)
     # =======================================================================================
     if 'shift' in LOGS_TO_USE[0] or 'clad' in LOGS_TO_USE[0]:
-        print('yep')
         plot_plot_per_timeofday_acc(results, domains, args)
+    # # =======================================================================================
+    # if 'frozen' in ''.join(LOGS_TO_USE):
+    #     frozen_related_results_plot(per_domain_avg_accs_train, domains, args)
+
 
 if __name__ == "__main__":
     args = parse_args()
