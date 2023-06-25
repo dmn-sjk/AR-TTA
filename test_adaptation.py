266fb3ca41eed90c8401ce044d8aa6c922a1d0a2
dmn-sjk <sojka.damian9@gmail.com>
2023-06-25 17:56:26 +0200

    shuffle cifar and turn off eval

--- test_adaptation.py
+++ test_adaptation.py
@@ -31,22 +31,27 @@ def main():
         with open(os.path.join(get_experiment_folder(cfg), experiment_name + '_config.yaml'), 'w') as f:
             yaml.dump(cfg, f, default_flow_style=False)
 
+    if cfg['dataset'] == 'cifar10c':
+        shuffle = True
+    else:
+        shuffle = False
+
     for i, experience in enumerate(benchmark.train_stream):
         # strategy.train(experience, eval_streams=[benchmark.streams['val']], shuffle=False,
         #                num_workers=cfg['num_workers)
 
-        if i == 0:
-            print("Initial eval...")
-            strategy.eval(benchmark.test_stream[0], num_workers=cfg['num_workers'])
+        # if i == 0:
+        #     print("Initial eval...")
+        #     strategy.eval(benchmark.test_stream[0], num_workers=cfg['num_workers'])
 
-        strategy.train(experience, eval_streams=[], shuffle=False,
+        strategy.train(experience, eval_streams=[], shuffle=shuffle,
                        num_workers=cfg['num_workers'])
 
         print(f"Percentage of used samples: {(strategy.model.num_samples_update / len(experience.dataset)) * 100.0:.2f}")
         
         strategy.model.num_samples_update = 0
 
-        strategy.eval(benchmark.test_stream[0], num_workers=cfg['num_workers'])
+        # strategy.eval(benchmark.test_stream[0], num_workers=cfg['num_workers'])
 
 
 if __name__ == '__main__':
