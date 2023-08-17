e15f2fd3ede8a166c266771fbaf9553e13b3acc6
dmn-sjk <sojka.damian9@gmail.com>
2023-08-17 18:33:46 +0000

    added resnet50 pretrained on imagenet from robust bench

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -21,7 +21,11 @@ def get_strategy(cfg):
     if cfg['model'] == 'resnet18':
         model = torchvision.models.resnet18(weights=torchvision.models.ResNet18_Weights.DEFAULT)
     elif cfg['model'] == 'resnet50':
-        model = torchvision.models.resnet50(weights=torchvision.models.ResNet50_Weights.DEFAULT)
+        if cfg['dataset'] == 'imagenetc':
+            model = load_model('Standard_R50', cfg['model_ckpt_dir'],
+                            'imagenet', "corruptions")
+        else:
+            model = torchvision.models.resnet50(weights=torchvision.models.ResNet50_Weights.DEFAULT)
     elif cfg['model'] == 'wideresnet28':
         # if 'cifar10' not in cfg['dataset']
             # raise ValueError(f"Robust bench wideresnet28 pretrained model only available for cifar10 dataset")
@@ -38,7 +42,8 @@ def get_strategy(cfg):
     else:
         raise ValueError(f"Unknown model name: {cfg['model']}")
 
-    if not (cfg['dataset'] == 'cifar10c' and cfg['model'] == 'wideresnet28'):    
+    if not (cfg['dataset'] == 'cifar10c' and cfg['model'] == 'wideresnet28' \
+        or cfg['dataset'] == 'imagenetc' and cfg['model'] == 'resnet50'):    
         model.fc = torch.nn.Linear(model.fc.in_features, cfg['num_classes'], bias=True)
 
     if 'pretrained_model_path' in cfg.keys():
